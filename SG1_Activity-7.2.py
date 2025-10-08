b6e430066f8db1c815b3229c9a51d2e726403a8e
ArrianeADass <arriane.dass@smc.pshs.edu.ph>
2025-10-08 19:37:40 +0800

    Update SG1_Activity-7.2.py
    ver. 1.0.0.1

--- SG1_Activity-7.2.py
+++ SG1_Activity-7.2.py
@@ -1,6 +1,6 @@
 def Delivery_Fee_Calculator(dist, rate):
     Delivery_Fee = dist * rate
-    print("Total Delivery Fee:", Delivery_Fee)
+    print("Total Delivery Fee:", round(Delivery_Fee, 2))
     
 dist = float(input("Enter distance in kilometers:"))
 rate = float(input("Enter rate per kilometer:"))
